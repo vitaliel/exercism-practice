bf7352b2f5e75df9c0918d0ccf58b2fd1abe3b41
Vitalie Lazu <vitalie.lazu@gmail.com>
2018-07-30 23:55:22 +0300

    Added elixir/nucleotide-count solution

---
+++ elixir/nucleotide-count/nucleotide_count.exs
@@ -0,0 +1,36 @@
+defmodule NucleotideCount do
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  @doc """
+  Counts individual nucleotides in a DNA strand.
+
+  ## Examples
+
+  iex> NucleotideCount.count('AATAA', ?A)
+  4
+
+  iex> NucleotideCount.count('AATAA', ?T)
+  1
+  """
+  @spec count([char], char) :: non_neg_integer
+  def count(strand, nucleotide) do
+    histogram(strand)[nucleotide]
+  end
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  iex> NucleotideCount.histogram('AATAA')
+  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+  """
+  @spec histogram([char]) :: map
+  def histogram(strand) do
+    result = List.foldl(@nucleotides, %{}, fn el, acc -> Map.put(acc, el, 0) end)
+
+    List.foldl(strand, result, fn el, acc ->
+      %{acc | el => acc[el] + 1}
+    end)
+  end
+end
