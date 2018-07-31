c90416c83a8d7507eebc112d204bc4f6e025eebe
Vitalie Lazu <vitalie.lazu@gmail.com>
2018-07-31 23:31:29 +0300

    Rework python/sum-of-multiples
    Use numbers_set.update(range(m, limit, m))

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -2,7 +2,6 @@ def sum_of_multiples(limit, multiples):
   numbers = set()
 
   for m in multiples:
-    for i in range(m, limit, m):
-      numbers.add(i)
+    numbers.update(range(m, limit, m))
 
   return sum(numbers)
