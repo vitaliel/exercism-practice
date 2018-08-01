74d52503f51015889a63ee24b5e7d069d19517d8
Vitalie Lazu <vitalie.lazu@gmail.com>
2018-08-01 22:10:07 +0300

    Elixir/bob: rework with case

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,25 +1,15 @@
-# Ported my python solution https://exercism.io/tracks/python/exercises/bob/solutions/46b976ab3d844677b10dbe9f02fec1d9
 defmodule Bob do
   def hey(input) do
     text = String.trim(input)
-    is_yelling = text == String.upcase(text) and text != String.downcase(text)
+    yelling? = text == String.upcase(text) and text != String.downcase(text)
+    q? = String.ends_with?(text, "?")
 
-    cond do
-      text == "" ->
-        "Fine. Be that way!"
-
-      String.ends_with?(text, "?") ->
-        if is_yelling do
-          "Calm down, I know what I'm doing!"
-        else
-          "Sure."
-        end
-
-      is_yelling ->
-        "Whoa, chill out!"
-
-      true ->
-        "Whatever."
+    case {text, yelling?, q?} do
+      {"", _, _} -> "Fine. Be that way!"
+      {_, true, true} -> "Calm down, I know what I'm doing!"
+      {_, false, true} -> "Sure."
+      {_, true, _} -> "Whoa, chill out!"
+      _ -> "Whatever."
     end
   end
 end
